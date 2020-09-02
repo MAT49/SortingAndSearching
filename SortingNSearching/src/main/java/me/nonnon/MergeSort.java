ef3f728affab7b2f357872dc1053d8dbc26428a7
Ha Hwang <spyderthing@gmail.com>
2020-09-01 23:13:48 -0400

    Added merge method.

--- SortingNSearching/src/main/java/me/nonnon/MergeSort.java
+++ SortingNSearching/src/main/java/me/nonnon/MergeSort.java
@@ -16,7 +16,33 @@ public class MergeSort {
 		}
 	}
 	
-	void merge() {
+	void merge(int[] array, int[] helper, int low, int middle, int high) {
+		// Copy both halves into a helper array
+		for (int i = low; i <= high; i++) {
+			helper[i] = array[i];
+		}
+		
+		int helperLeft = low;	// location pointer for left half
+		int helperRight = middle + 1;	// location pointer for right half
+		int current = low;	// location pointer for the final array
+		
+		// Iterate through helper array. Compare left and right half, 
+		// copying back the smaller element front he two halves into the original array
+		while (helperLeft <= middle && helperRight <= high) {
+			if (helper[helperLeft] <= helper[helperRight]) {
+				array[current] = helper[helperLeft];
+				helperLeft++;
+			} else {	// that is, Right element <= Left element
+				array[current]  = helper[helperRight];
+			}
+			current++;
+		}
+		
+		// Copy the rest of the left array into the final array
+		int remaining = middle - helperLeft;
+		for (int i = 0; i <= remaining; i++) {
+			array[current + i] = helper[helperLeft + 1];
+		}
 		
 	}
 
