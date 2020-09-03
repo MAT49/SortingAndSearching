903f4677b9c479cc80773fecaf58098a3a2c22ff
Ha Hwang <spyderthing@gmail.com>
2020-09-02 23:23:07 -0400

    Added BinarySearch class.

---
+++ SortingNSearching/src/main/java/me/nonnon/BinarySearch.java
@@ -0,0 +1,35 @@
+package me.nonnon;
+
+public class BinarySearch {
+	int binarySearch(int[] a, int x) {
+		int low = 0;
+		int high = a.length - 1;
+		int mid;
+		
+		while (low <= high) {
+			mid = (low + high) / 2;
+			if (a[mid] < x) {
+				low = mid + 1;
+			} else if (a[mid] > x) {
+				high = mid - 1;
+			} else {
+				return mid;
+			}
+		}
+		return -1;	// Error, no match found
+	}
+	
+	int binarySearchRecursive(int[] a, int x, int low, int high) {
+		if (low > high) return -1;	// Error, no match found
+		
+		int mid = (low + high) / 2;
+		if (a[mid] < x) {
+			return binarySearchRecursive(a, x, mid + 1, high);
+		} else if (a[mid] > x) {
+			return binarySearchRecursive(a, x, low, mid - 1);
+		} else {
+			return mid;
+		}
+	}
+
+}
